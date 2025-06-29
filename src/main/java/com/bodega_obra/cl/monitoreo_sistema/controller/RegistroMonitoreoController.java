425def86f976307f516136b4551c13143c06c4d2
Ricardo Díaz <ric.diazs@duocuc.cl>
2025-06-29 02:30:15 -0400

    Se agrega documentacion OAS a microservicio
    Con la creacion de la clase 'SwaggerConfig' y la modificacion del
    controller, se documenta la API del microservicio usando el estandar OAS
    y es posible mostrarla en la web en un formato amigable con Swagger.

--- src/main/java/com/bodega_obra/cl/monitoreo_sistema/controller/RegistroMonitoreoController.java
+++ src/main/java/com/bodega_obra/cl/monitoreo_sistema/controller/RegistroMonitoreoController.java
@@ -17,13 +17,35 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bodega_obra.cl.monitoreo_sistema.model.RegistroMonitoreo;
 import com.bodega_obra.cl.monitoreo_sistema.service.RegistroMonitoreoService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/api/v1/registros-monitoreo")
+@Tag(name = "Registros de Monitoreo", description = "Operaciones CRUD en registros del microservicio de monitoreo de sistema.")
 public class RegistroMonitoreoController {
     @Autowired
     private RegistroMonitoreoService registroMonitoreoService;
 
     @GetMapping
+    @Operation(summary = "Obtener todos los registros.", description = "Regresa todos los registros del microservicio de monitoreo de sistema.")
+    @ApiResponses(
+        value = {
+            @ApiResponse(
+                responseCode = "200",
+                description = "Se han encontrado todos los registros del monitoreo.",
+                content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = RegistroMonitoreo.class)
+                )
+            ),
+            @ApiResponse(responseCode = "204", description = "No hay registros de monitoreo.")
+        }
+    )
     public ResponseEntity<List<RegistroMonitoreo>> enlistarRegistrosMonitoreo() {
         List<RegistroMonitoreo> registrosMonitoreo = registroMonitoreoService.findAll();
 
@@ -35,6 +57,20 @@ public class RegistroMonitoreoController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Obtener un registro.", description = "Se obtiene un registro del microservicio mediante su número de ID.")
+    @ApiResponses(
+        value = {
+            @ApiResponse(
+                responseCode = "200",
+                description = "El registro se encontró exitosamente.",
+                content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = RegistroMonitoreo.class)
+                )
+            ),
+            @ApiResponse(responseCode = "404", description = "Registro no encontrado.")
+        }
+    )
     public ResponseEntity<RegistroMonitoreo> buscarRegistroMonitoreo(@PathVariable Integer id) {
         try {
             RegistroMonitoreo registroMonitoreo = registroMonitoreoService.findById(id);
@@ -45,12 +81,40 @@ public class RegistroMonitoreoController {
     }
 
     @PostMapping
+    @Operation(summary = "Crear un nuevo registro.", description = "Se crea un nuevo registro en el microservicio.")
+    @ApiResponses(
+        value = {
+            @ApiResponse(
+                responseCode = "201",
+                description = "El registro se ha creado exitosamente.",
+                content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = RegistroMonitoreo.class)
+                )
+            ),
+            @ApiResponse(responseCode = "400", description = "No se pudo crear el registro.")
+        }
+    )
     public ResponseEntity<RegistroMonitoreo> crearRegistroMonitoreo(@RequestBody RegistroMonitoreo registroMonitoreo) {
         RegistroMonitoreo nuevoRegistro = registroMonitoreoService.save(registroMonitoreo);
         return ResponseEntity.status(HttpStatus.CREATED).body(nuevoRegistro);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Actualizar un registro.", description = "Se actualiza un registro del microservicio a partir de su número de ID.")
+    @ApiResponses(
+        value = {
+            @ApiResponse(
+                responseCode = "200",
+                description = "El registro se ha actualizado exitosamente.",
+                content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = RegistroMonitoreo.class)
+                )
+            ),
+            @ApiResponse(responseCode = "404", description = "Registro no encontrado.")
+        }
+    )
     public ResponseEntity<RegistroMonitoreo> actualizarRegistroMonitoreo(@PathVariable Integer id, @RequestBody RegistroMonitoreo registroMonitoreo) {
         try {
             RegistroMonitoreo registroModificar = registroMonitoreoService.findById(id);
@@ -69,6 +133,19 @@ public class RegistroMonitoreoController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Eliminar un registro.", description = "Se elimina un registro del microservicio a partir de su número de ID.")
+    @ApiResponses(
+        value = {
+            @ApiResponse(
+                responseCode = "204",
+                description = "El registro se ha eliminado exitosamente."
+            ),
+            @ApiResponse(
+                responseCode = "404",
+                description = "Registro no encontrado."
+            )
+        }
+    )
     public ResponseEntity<?> eliminarRegistroMonitoreo(@PathVariable Integer id) {
         try {
             registroMonitoreoService.delete(id);
